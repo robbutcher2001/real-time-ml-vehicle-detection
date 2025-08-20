e8d3821632ee67c9b20ea7d94d6b78815b8d74c7
Rob Butcher <robbutcher2001@gmail.com>
2025-08-20 21:40:18 +0100

    Inference from stream + task queue

---
+++ src/inference.py
@@ -0,0 +1,44 @@
+from ultralytics import YOLO
+from rtsp_stream import get_frame
+
+def isIntersectingKitchen(x, y):
+    x1 = 1550 #top-left
+    y1 = 650 #top-left
+    x2 = 2100 #bottom-right
+    y2 = 1200 #bottom-right
+    xBound = x > x1 and x < x2
+    yBound = y > y1 and y < y2
+
+    return True if xBound and yBound else False
+
+def isIntersectingFrontdoor(x, y):
+    x1 = 850 #top-left
+    y1 = 450 #top-left
+    x2 = 1400 #bottom-right
+    y2 = 1000 #bottom-right 
+    xBound = x > x1 and x < x2
+    yBound = y > y1 and y < y2
+
+    return True if xBound and yBound else False
+
+def car_space_inference():
+    kitchen_occupied = False
+    frontdoor_occupied = False
+
+    model = YOLO("yolo11x.pt")
+    results = model(get_frame(), device="mps", verbose=False)
+
+    for result in results:
+        boxes = result.boxes
+        xywh = boxes.xywh
+        for detection in xywh:
+            x = int(detection[0].tolist()) #TODO: check tolist()
+            y = int(detection[1].tolist())
+            if isIntersectingKitchen(x, y):
+                kitchen_occupied = True
+            if isIntersectingFrontdoor(x, y):
+                frontdoor_occupied = True
+    
+    print("Kitchen space is occupied" if kitchen_occupied else "Kitchen space is free")
+    print("Frontdoor space is occupied" if frontdoor_occupied else "Frontdoor space is free")
+    # Upload to cloud storage here
---
+++ src/task_queue_thread.py
@@ -0,0 +1,42 @@
+from flask import Flask, jsonify
+from inference import car_space_inference
+import threading
+import queue
+import time
+import uuid
+
+INFERENCES_PER_TASK = 3
+INFERENCE_INTERVAL = 60 * 2
+
+app: Flask = Flask(__name__)
+inference_queue: queue.Queue = queue.Queue()
+
+def process_task(task):
+    print(f"Inference {task['id']} started")
+    for _ in range(INFERENCES_PER_TASK):
+        car_space_inference()
+        time.sleep(INFERENCE_INTERVAL)
+    print(f"Inference {task['id']} completed")
+
+def worker():
+    print("Worker started, waiting for inference requests..")
+    while True:
+        try:
+            process_task(inference_queue.get())
+        finally:
+            inference_queue.task_done()
+            print(f"Queue size {inference_queue.qsize()}" if inference_queue.qsize() > 0 else "Queue is empty, waiting for inference requests..")
+
+@app.post("/inference")
+def inference():
+    id = str(uuid.uuid4())
+    queue_size = inference_queue.qsize() + 1
+    inference_queue.put({ 'id': id })
+    print(f"Inference {id} queued, queue size: {queue_size}")
+    return jsonify({"queued": id, "queue_position": queue_size})
+
+if __name__ == "__main__":
+    threading.Thread(target=worker, daemon=True).start()
+    app.run(host="0.0.0.0", port=8000)
+
+# need to run from terminal with: gunicorn -w 4 -b 0.0.0.0:8000 app:app
