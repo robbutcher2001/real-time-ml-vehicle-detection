9fe62a0eb2e369f21e319356d49054017faac88c
Rob Butcher <robbutcher2001@gmail.com>
2025-11-13 22:58:53 +0000

    Layout

---
+++ ui/src/views/ParkingView.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import '../assets/parking.css'
+</script>
+
+<template>
+  <div class="page dev">
+    <h1 class="card-heading">Parking</h1>
+    <div role="doc-subtitle">Carriage Walk</div>
+    <hr />
+    <div class="spaces">
+      <div class="space">Space A</div>
+      <div class="space">Space B</div>
+    </div>
+  </div>
+</template>
