8e20a0f9f7b9002bf9243921e33e8ac7bc294514
Rob Butcher <robbutcher2001@gmail.com>
2025-11-22 20:48:58 +0000

    Layout and styles

--- ui/src/views/ParkingView.vue
+++ ui/src/views/ParkingView.vue
@@ -3,13 +3,23 @@ import '../assets/parking.css'
 </script>
 
 <template>
-  <div class="page dev">
-    <h1 class="card-heading">Parking</h1>
+  <div class="page">
+    <h1>Parking</h1>
     <div role="doc-subtitle">Carriage Walk</div>
     <hr />
     <div class="spaces">
-      <div class="space">Space A</div>
-      <div class="space">Space B</div>
+      <div class="space occupied">
+        <h2>Front Door</h2>
+        <span>
+          <div class="text">Occupied</div>
+        </span>
+      </div>
+      <div class="space vacant">
+        <h2>Kitchen</h2>
+        <span>
+          <div class="text">Vacant</div>
+        </span>
+      </div>
     </div>
   </div>
 </template>
